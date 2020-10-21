931ca46648ab9cfe72aad4b7e4b3d64d9c7e1b33
kevinkirkl3y <kevinmkirkley@gmail.com>
2020-10-21 11:02:19 -0700

    Added jquery functions to show/hide parts of the letter.
    Co-authored-by: Maxi Benbassat <info@benbassatdigital.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  $("#formOne")
-})
+  $("#formOne").submit(function(){
+    const constituentNameInput = $("input#constituent-name").val();
+
+    $(".constituent-name").append(constituentNameInput);
+
+    $("#letter-filled").show();
+    $("#letter-unfilled").hide();
+    event.preventDefault();
+  })
+});
